95d5f3edcfe02db7ab680951a20595121eb5db8c
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-04-13 12:02:10 -0300

    Projeto Blog Pessoal

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,65 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens") /*Create TABLE tb_postagens*/
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O Atributo título é obrigatório!")
+	@Size(min= 5, max= 100, message = "O Atributo título deve ter no minimo 5 e no maximo 100 caracters!")
+	private String titulo;
+	@NotBlank(message = "O Atributo título é obrigatório!")
+	@Size(min= 10, max= 1000, message = "O Atributo título deve ter no minimo 10 e no maximo 1000 caracters!")
+	private String texto;
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+}
