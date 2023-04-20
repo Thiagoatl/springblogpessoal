2c670502b8c1396524997283e4c2e956bc480ece
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-04-19 22:17:32 -0300

    Loja update

--- gameloja/src/main/java/model/Produto.java
+++ gameloja/src/main/java/model/Produto.java
@@ -1,42 +1,111 @@
 package model;
 import java.math.BigDecimal;
-import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_produtos")
+@Table (name = "tb_produtos")
 public class Produto {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull(message = "O atributo nome é obrigatório")
+	@NotBlank(message = "O atributo nome é obrigatório") 
 	private String nome;
 	
-	@NotNull(message = "O Atributo Descrição é obrigatório")
+	@Size(max = 500)
 	private String descricao;
 	
-	@NotNull(message = "O atributo preço é obrigatório")
-	@PositiveOrZero
+	@NotBlank(message = "O atributo console é obrigatório") 
+	private String console;
+	
+	@NotNull 
+	private Long quantidade;
+	
+	@NotNull(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior do que zero!")
 	private BigDecimal preco;
 	
 	private String foto;
 	
-	@NotNull(message = "O atributo platagorma é obrigatório")
-	private String plataforma;
-	
-	@NotNull(message = "O atributo data é obrigatório")
-	private Date data_lancamento;
+	//relacionamento
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 	
+
 }
