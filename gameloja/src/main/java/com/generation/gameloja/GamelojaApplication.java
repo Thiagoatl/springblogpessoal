5bacc69dc3c196862aec08af114518338bb6653f
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-04-19 22:10:03 -0300

    Gameloja

---
+++ gameloja/src/main/java/com/generation/gameloja/GamelojaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.gameloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelojaApplication.class, args);
+	}
+
+}
---
+++ gameloja/src/test/java/com/generation/gameloja/GamelojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gameloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
